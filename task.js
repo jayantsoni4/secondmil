a8dfbffc526b055dec9f07dc08108ef9a4a2c8e9
Jayant <sonikhush980@gmail.com>
2024-12-31 12:17:04 +0530

    Add backend and frontend code

---
+++ task.js
@@ -0,0 +1,15 @@
+// const mongoose = require('mongoose');
+
+// const taskSchema = new mongoose.Schema({
+//   name: String,
+//   location: String,
+//   phone: String,
+//   date: String,
+//   notes: String,
+//   category: String,
+//   images: [String], // You can store image URLs if you're uploading images to a server or use base64 encoding
+// });
+
+// const Task = mongoose.model('Task', taskSchema);
+
+// module.exports = Task;
