aea9835c2568c14fee8b62718d1cc10da486dcea
jayantsoni4 <160615001+jayantsoni4@users.noreply.github.com>
2025-01-02 10:10:13 +0530

    Update data.js

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); // Serve static files for CSS
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/todoAppDB", {
+mongoose.connect("mongodb://127.0.0.1:27017/curd", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
